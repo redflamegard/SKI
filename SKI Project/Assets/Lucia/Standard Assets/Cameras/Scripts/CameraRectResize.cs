5caa747e8b1338a1f6b12a380faced3686cd80e8
chris-s-harper <chris.harper@loop.colum.edu>
2017-09-21 18:09:55 -0500

    Implimented camera follow script
    Loaded the follow script into the scene, added my test scene, added
    conditions so the camera only follows the y position once the distance
    reaches a certain point

---
+++ SKI Project/Assets/Lucia/Standard Assets/Cameras/Scripts/CameraRectResize.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//goes on camera
+//pays attention to when other cameras should be disabled to resize rect transform
+public class CameraRectResize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
