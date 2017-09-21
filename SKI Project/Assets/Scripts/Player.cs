3653fe30fa6c7db387de9dffc057bd018c61ebd3
redflamegard <Corey.King@iam.local>
2017-09-21 16:15:26 -0500

    Update

---
+++ SKI Project/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void PlayerDieDelegate();
+
+public class Player : MonoBehaviour {
+    Color carColor;
+    GameObject modelOfCar;
+    bool isAlive;
+    float currentDamage;
+    string playerName;
+    PlayerID playerID;
+
+
+    private void PlayerSpawn()
+    {
+        PlayerManager.playersInScene[PlayerManager.playersInScene.Length] = new PlayerData(playerName, carColor, modelOfCar, playerID);
+        isAlive = true;
+        currentDamage = 0f;
+    }
+
+    private void GrabPickUp(GameObject pickUp)
+    {
+        //Instantiate shtuffs
+        PlayerManager.AddPowerUpToPlayer(pickUp, playerID);
+
+    }
+}
+
+
---
+++ SKI Project/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+    public static PlayerData[] playersInScene;
+
+    public static event Action<GameObject, PlayerID> GrabPowerUp;
+
+    public static void AddPowerUpToPlayer(GameObject powerUp, PlayerID id)
+    {
+        GrabPowerUp += PlayerManager_GrabPowerUp;
+    }
+
+    private static void PlayerManager_GrabPowerUp(GameObject powerUpToAdd, PlayerID id)
+    {
+        foreach (var player in playersInScene)
+        {
+            if (id == player.playerID)
+            {
+                player.AddPowerUp(powerUpToAdd);
+            }
+        }
+    }
+}
+
+
+public enum PlayerID { one, two, three, four };
+
+public class PlayerData
+{
+    public PlayerID playerID;
+    GameObject currentPowerUp;
+    Color carColor;
+    GameObject modelOfCar;
+    int roundsWon;
+    int livesLeft;
+
+    string playerName;
+
+    public void AddPowerUp(GameObject powerUpObj)
+    {
+        currentPowerUp = powerUpObj;
+    }
+
+    public PlayerData(string name, Color colorOfCar, GameObject carModel, PlayerID playerID)
+    {
+        carColor = colorOfCar;
+        modelOfCar = carModel;
+        playerName = name;
+        this.playerID = playerID;
+    }
+}
