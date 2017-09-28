4f3256d268e7a4b739ee5988cfe65a6785dc73fb
thomas-mahon <thomas.mahon@loop.colum.edu>
2017-09-28 13:07:13 -0500

    Cannon added

---
+++ SKI Project/Assets/Scripts/CannonController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour {
+    public PlayerID currentPlayerID;
+    [SerializeField]
+    float rotationSpeed;
+    [SerializeField]
+    float aimSpeed;
+    [SerializeField]
+    GameObject cannonBallPrefab;
+    [SerializeField]
+    Transform shotPoint;
+    [SerializeField]
+    float shotForce;
+
+    private bool isRotating = false;
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+        {
+            isRotating = true;
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject shot = Instantiate(cannonBallPrefab, shotPoint.position, Quaternion.identity);
+        shot.GetComponent<Rigidbody>().AddForce(0f, 0f, shotForce);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRotating)
+        {
+            RotateCannon();
+        }
+        
+    }
+    
+    private void RotateCannon()
+    {
+        float lookRotation = transform.localEulerAngles.y + Mathf.Clamp(Input.GetAxis("Horizontal"), -1f, 1f) * rotationSpeed * Time.deltaTime;
+        float aimRotation = transform.localEulerAngles.x + Mathf.Clamp(Input.GetAxis("Vertical"), -1f, 1f) * aimSpeed * Time.deltaTime;
+        
+        transform.localEulerAngles = new Vector3(aimRotation, lookRotation, 0);
+    }
+}
---
+++ SKI Project/Assets/Scripts/CannonTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonTrigger : MonoBehaviour {
+    [SerializeField]
+    Transform holdLocation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CannonController controller = GetComponent<CannonController>();
+        if (other.GetComponent<Player>().GetType() == typeof(Player))
+        {
+            other.GetComponent<Player>().enabled = false;
+            other.transform.position = holdLocation.position;
+
+            //controller.currentPlayerID = other.GetComponent<Player>().ID;
+            controller.enabled = true;
+        }
+    }
+}
