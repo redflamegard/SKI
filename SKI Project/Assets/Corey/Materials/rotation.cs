1d755e39de2d986b4ad64a7b204e17fa8a7b2a45
redflamegard <corey.king@loop.colum.edu>
2017-09-28 17:58:27 -0500

    CoreysClasswork

---
+++ SKI Project/Assets/Corey/Materials/rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        this.gameObject.transform.Rotate(2,5,5);
+		
+	}
+}
