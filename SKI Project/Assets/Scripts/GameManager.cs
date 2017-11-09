a393f6616fa866207d634d899d8cdff40520917a
dboggsIAM <Dylan.Boggs@iam.local>
2017-11-09 13:46:40 -0600

    Dylan Worked on Timer Shit

--- SKI Project/Assets/Scripts/GameManager.cs
+++ SKI Project/Assets/Scripts/GameManager.cs
@@ -8,10 +8,52 @@ public class GameManager : MonoBehaviour {
     GameObject endRoundPanel;
     [SerializeField]
     Text winningPlayerText;
+    [SerializeField]
+    Text countDownText;
+
+    int countDownCount = 3;
+
+    public void Start()
+    {
+        countDownText.text = countDownCount.ToString();
+        //StartCoroutine(CountDown());   
+    }
 
     public void EndGame(PlayerID winningPlayerID)
     {
         //winningPlayerText.text += winningPlayerID + 1;
         //endRoundPanel.SetActive(true);
     }
+
+
+    public void GameStartCountDown()
+    {
+        if (countDownCount == 0)
+        {
+            foreach (GameObject players in PlayerManager.vehiclesInScene)
+            {
+                players.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+    }
+
+    public IEnumerator CountDown1()
+    {
+        if (countDownCount == 3)
+        {
+            yield return WaitForSeconds(1);
+            countDownCount--;
+            StartCoroutine(CountDown2());
+        }
+    }
+
+    public IEnumerator CountDown2()
+    {
+        if (countDownCount == 2)
+        {
+            yield return countDownCount--;
+        }
+    }
+
+
 }
