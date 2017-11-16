0929840774a003c95a96e9bc904f1c22984fabfb
alexgraner <alexgraner@yahoo.com>
2017-11-16 14:22:17 -0600

    MayanTempleWhitebox
    Alex's whitebox of Mayan Temple level, scripting not all working but geometry there

---
+++ SKI Project/Assets/Alex Stuff/Scripts/HeadTriggers.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTriggers : MonoBehaviour {
+
+    //cars
+    public GameObject car1;
+    public GameObject car2;
+    //public GameObject car3;
+    //public GameObject car4;
+
+    public GameObject headObj;//gameobject that will fall after a player enters a trigger
+    Vector3 ogHeadLoc;//where head will placed when it is not down, set to where headObj starts
+
+    bool canFall;//State if head can be dropped, when true, trigger is active
+    bool playerInTrigger;//if player has entered trigger, head will fall, turning "canFall" false
+
+    public float timeBeforeFall;//how long after player enters trigger will head fall
+    public float timeHeadIsDown;//how long head will remain down before going back up, acts as a cooldown time too
+    float maxTimeBeforeFall;//holds default timer to revert back to each time
+    float maxTimeHeadIsDown;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        canFall = true;
+        ogHeadLoc = headObj.transform.position;
+
+        maxTimeBeforeFall = timeBeforeFall;
+        maxTimeHeadIsDown = timeHeadIsDown;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(!canFall)
+        {
+            timeBeforeFall -= Time.deltaTime;
+
+            if(timeBeforeFall <= 0)
+            {
+                headObj.transform.position = this.transform.position;
+
+                timeHeadIsDown -= Time.deltaTime;
+
+                if(timeHeadIsDown <= 0)
+                {
+                    headObj.transform.position = ogHeadLoc;
+                    canFall = true;
+
+                    timeBeforeFall = maxTimeBeforeFall;
+                    timeHeadIsDown = maxTimeHeadIsDown;
+                }
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(canFall)
+        {
+            if(other.gameObject == car1 || other.gameObject == car2)//had other.GameObject.name == "Player1", didnt work either
+            {
+                print("in trigger");//not working
+
+                canFall = false;
+            }
+        }
+    }
+}
---
+++ SKI Project/Assets/Alex Stuff/Scripts/Rotate.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float speed;
+
+    public float rotateMode = 0;
+
+    public bool lightHouseDependant = false;
+    public GameObject levelControl;
+    //public PowerPlantLevel powerPlantLevel;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //levelControl = GameObject.Find("LevelControl");
+        //if (lightHouseDependant)
+        //{
+        //    powerPlantLevel = levelControl.gameObject.GetComponent<PowerPlantLevel>();
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //if (lightHouseDependant)
+        //{
+
+        //        if (rotateMode == 0)
+        //        {
+        //            transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        //        }
+        //        else if (rotateMode == 1)
+        //        {
+        //            transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        //        }
+        //        else if (rotateMode == 2)
+        //        {
+        //            transform.Rotate(Vector3.right * Time.deltaTime * speed);
+        //        }
+
+        //}
+        //else
+        //{
+        if (rotateMode == 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        }
+        else if (rotateMode == 1)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else if (rotateMode == 2)
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * speed);
+        }
+        else if (rotateMode == 3)
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * speed);
+            transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+            transform.Rotate(Vector3.up * Time.deltaTime * speed);
+        }
+        //    }
+        //}
+    }
+}
