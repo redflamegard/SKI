577a74f19720183b92e91b96105004aadc1d383a
alexgraner <alexgraner@yahoo.com>
2017-12-19 08:49:07 -0600

    Alex Progress on 4-3
    Finishes Construction area and starts first section of Office building

---
+++ SKI Project/Assets/Persons Work Folders/Alex Stuff/Scripts/DongCollider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DongCollider : MonoBehaviour {
+
+	public DongExplosion dongExplosion;
+
+	bool canCoolDown;
+
+	public float explosionCoolDownTimer;// how long until canExplode can be triggered again
+	float maxTimer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		maxTimer = explosionCoolDownTimer;
+
+		dongExplosion = GetComponentInChildren<DongExplosion> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		//FOR IF WE WANT A TIMER?
+		//if (canCoolDown) 
+		//{
+			//explosionCoolDownTimer -= Time.deltaTime;
+
+			//if (explosionCoolDownTimer <= 0) 
+			//{
+				//dongExplosion
+				//canCoolDown = false;
+				//explosionCoolDownTimer = maxTimer;
+			//}
+	}	
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			dongExplosion.CreateExplosion ();
+
+			//dongExplosion.canExplode = true;
+
+			//canCoolDown = true;
+		}
+	}
+}
---
+++ SKI Project/Assets/Persons Work Folders/Alex Stuff/Scripts/DongExplosion.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DongExplosion : MonoBehaviour {
+
+	List<GameObject> carsInRadius = new List<GameObject> ();//holds all cars currently in sphere for explosion force to be added to
+
+	public bool canExplode;//Dong script tells this to trigger explosion when car hits dong collider
+
+	public float radius = 5.0f;
+	public float power = 10.0f;
+
+	Vector3 explosionPos;
+
+	void Start()
+	{
+		explosionPos = this.transform.position;//origin point for explosion center of dong with this, right?
+		//Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+	}
+
+	void Update () 
+	{
+		//if (canExplode) 
+		//{
+			//foreach (GameObject g in carsInRadius)
+			//{
+				//Rigidbody rb = g.GetComponentInParent<Rigidbody>();
+
+				//rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+			//}
+	}
+
+	public void CreateExplosion()
+	{
+		foreach (GameObject g in carsInRadius)
+		{
+			Rigidbody rb = g.GetComponentInParent<Rigidbody>();
+
+			rb.AddExplosionForce(power, explosionPos, radius, 3f);
+		}
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			print ("car in");
+			carsInRadius.Add (other.gameObject);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		print ("car out");
+		if (other.gameObject.tag == "Player") 
+		{
+			carsInRadius.Remove(other.gameObject);
+		}
+	}
+}
